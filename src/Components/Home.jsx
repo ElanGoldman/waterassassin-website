2474174f54ec7bbe710b37a67873cab86b0424db
ElanGoldman <elangoldman101@gmail.com>
2025-09-12 11:29:09 -0400

    Edited defaults

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="page">
             <h2>Home</h2>
-            <p>This is the website for OCP water assassin.</p>
+            <p>This is the website for OCP 💦🔫🔫💦 water assassin 💦🔫🔫💦</p>
 
             <img src={sample} alt="Sample" style={{maxWidth: '100%', height: 'auto'}} />
         </div>
