aeab3aa55d597fc3e5f6875767ba1309643629b8
ElanGoldman <elangoldman101@gmail.com>
2025-09-12 11:25:15 -0400

    Fixed things

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,12 @@
+import sample from '../Assets/sample.jpg';
+
 export default function Home() {
     return (
         <div className="page">
             <h2>Home</h2>
             <p>This is the website for OCP water assassin.</p>
+
+            <img src={sample} alt="Sample" style={{maxWidth: '100%', height: 'auto'}} />
         </div>
     );
 }
